f15561ecc941a5b3957876abb91c0d393fab48a0
MarineChap <marinechap@MacBook-Air-de-Chaput.local>
2018-11-08 22:19:09 +0100

    ECG_Patient1&2

--- ECG_filtering/Exercise2.m
+++ ECG_filtering/Exercise2.m
@@ -18,96 +18,66 @@ t    = (0:length(ecg)-1)/fs  ;   % Creating a Time Vector
 % signal to design the algorithm. Then, apply the method in the signals
 % from the patients.
 % Low-pass Filter
-close all
-B_lp_1 = [1 zeros(1,5) -2 zeros(1,5) 1]/32;
-A_lp_1 = [1 -2 1];
-ecg_lp = filter(B_lp_1,A_lp_1,ecg);
 
-% High-Pass Filter
-
-B_lp_2 = [1 zeros(1,31) -1];
-A_lp_2 = [1 -1];
-ecg_lp_2 = filter(B_lp_2,A_lp_2,ecg_lp);
-
-ecg_lp_ex = [zeros(16,1); ecg_lp];
-ecg_hp = ecg_lp_ex(1:end-16)-ecg_lp_2/32;
-
-% Derivative
-% implement the derivative filter of the Pan-Tomkins algorithm
-
-B_d = [2 1 0 -1 -2]; 
-A_d = [8 0 0 0 0]; 
-ecg_d = filter(B_d,A_d,ecg_lp_2);
-
-% Squaring
-% implement the squaring step in the Pan-Tomkins algorithm
-
-ecg_square = ecg_d.^2;
-
-% Integration
-% implement the integration step in the Pan-Tomkins algorithm
-B_int = ones(1,30);
-A_int = [30 zeros(1,29)];
-ecg_int = filter(B_int,A_int,ecg_square);
-
-% Detect initial peaks
-
-pos_top = find(ecg_int>0.5*max(ecg_int));
-
-% Searchback procedure and adaptive thresholding. Correct for delays if
-% needed. Use the provided function searchback.mat provided in toledo.
-Rpos=searchback(pos_top,ecg_int,fs);
-
-figure(8);
-subplot(511); 
-plot(t,ecg_clean);
-xlim([16,18]);
-subplot(512);
-plot(t,ecg_hp);
-xlim([16,18]);
-subplot(513);
-plot(t,ecg_d);
-xlim([16,18]);
-subplot(514);
-plot(t,ecg_square);
-xlim([16,18]);
-subplot(515);
-hold on;
-plot(t, ecg, t, ecg_int);
-stem(t(Rpos), ecg_int(Rpos));
-hold off;
-xlim([16,18]);
+Rpos = Pan_tompkins( ecg,fs );
 
 
 %%
 %   Detecting Rpeaks in the patients. Insert a piece of code to detect the
 %   R-peaks in the signals from the patients.
+close all; 
 
+[B_f2,A_f2] = Derivative_based_filter(0.984,fs);
+ECG_1 = (ECG_1 - mean(ECG_1))/std(ECG_1);
+ECG_1 = filter(B_f2,A_f2, ECG_1); 
+
+ECG_1_ds = ECG_1((1:5:length(ECG_1)-1));
+Rpos_1 = Pan_tompkins( ECG_1_ds,fs );
+
+t = (0:length(ECG_1_ds)-1)/fs;
+figure;
+hold on;
+plot(t,ECG_1_ds);
+stem(t(Rpos_1),ECG_1_ds(Rpos_1));
+hold off;
+legend('ECG','R-peaks')
+ylabel('ECG')
+axis tight;
+
+
+index_2 = 0.89*1000:17.23*1000;
+ECG_2 = ECG_2(index_2);
+ECG_2 = (ECG_2 - mean(ECG_2))/std(ECG_2);
+ECG_2_ds = ECG_2((1:5:length(ECG_2)-1));
+
+Rpos_2 = Pan_tompkins( ECG_2_ds,fs );
 
 
 %% Detection of the Dichrotic notch
 % Implement a piece of code to detect the Dicrotic notch in the signals In
 % this section.
 
-
+PCG_2 =PCG_2(index_2);
 %% Figure 1.1
 % A figure showing the steps to segment the PCG signals on its sistolic and
 % diastolic parts. See figure 4.30 in the textbook. The same figure should
 % be shown here. Do this for patient one here
-
+t1 = (0:length(PCG_1)-1)/1000;
 figure
 
 subplot 511                 
 
-
+plot(t1, PCG_1)
 legend('PCG signal','QRS complex location','Dicrotic notch location')
 ylabel('PCG')
 axis tight;
 
 
 subplot 512                 
-
-
+hold on;
+plot(t1, ECG_1);
+stem(t1(Rpos_1*1000/250),ECG_1(Rpos_1*1000/250));
+hold off;
 legend('ECG','R-peaks')
 ylabel('ECG')
 axis tight;
@@ -141,17 +111,21 @@ suptitle('Patient 1')
 % A figure showing the steps to segment the PCG signals on its sistolic and
 % diastolic parts. See figure 4.30 in the textbook. The same figure should
 % be shown here. Do this for patient two here
-
+t2 = (0:length(PCG_2)-1)/1000;
 figure
 
 subplot 511                 
 
+plot(t2, PCG_2)
 legend('PCG signal','QRS complex location','Dicrotic notch location')
 ylabel('PCG')
 axis tight;
 
-subplot 512                 
-
+subplot 512   
+hold on;
+plot(t2, ECG_2);
+stem(t2(Rpos_2*1000/250),ECG_2(Rpos_2*1000/250));
+hold off;
 legend('ECG','R-peaks')
 ylabel('ECG')
 axis tight;
---
+++ ECG_filtering/Pan_tompkins.m
@@ -0,0 +1,47 @@
+function [ Rpos ] = Pan_tompkins( ecg,fs )
+%PAN_TOMPKINS algorithm. 
+
+% Low-pass Filter
+
+B_lp_1 = [1 zeros(1,5) -2 zeros(1,5) 1]/32;
+A_lp_1 = [1 -2 1];
+ecg_lp = filter(B_lp_1,A_lp_1,ecg);
+
+% High-Pass Filter
+
+B_lp_2 = [1 zeros(1,31) -1];
+A_lp_2 = [1 -1];
+ecg_lp_2 = filter(B_lp_2,A_lp_2,ecg_lp);
+
+ecg_lp_ex = [zeros(16,1); ecg_lp];
+ecg_hp = ecg_lp_ex(1:end-16)-ecg_lp_2/32;
+
+% Derivative
+% implement the derivative filter of the Pan-Tomkins algorithm
+
+B_d = [2 1 0 -1 -2]; 
+A_d = [8 0 0 0 0]; 
+ecg_d = filter(B_d,A_d,ecg_hp);
+
+% Squaring
+% implement the squaring step in the Pan-Tomkins algorithm
+
+ecg_square = ecg_d.^2;
+
+% Integration
+% implement the integration step in the Pan-Tomkins algorithm
+B_int = ones(1,30);
+A_int = [30 zeros(1,29)];
+ecg_int = filter(B_int,A_int,ecg_square);
+
+% Detect initial peaks
+
+pos_top = find(ecg_int>0.5*max(ecg_int));
+
+% Searchback procedure and adaptive thresholding. Correct for delays if
+% needed. Use the provided function searchback.mat provided in toledo.
+Rpos=searchback(pos_top,ecg,fs);
+
+
+end
+
